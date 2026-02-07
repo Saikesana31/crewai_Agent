0caf0982a6d2553577425a6e73fb834fad0f969b
Saikesana31 <kesana.class2024@gmail.com>
2026-02-07 12:54:54 -0600

    setup venv and created files

---
+++ template.sh
@@ -0,0 +1,22 @@
+# create a Director 
+mkdir -p src
+mkdir -p src/config
+mkdir -p src/data
+mkdir -p src/tools
+
+
+# create a file 
+
+touch src/
+touch .env
+touch requirements.txt
+touch src/config/agents.yaml
+touch src/config/tools.yaml
+touch src/main.py
+touch src/crew.py
+touch src/__init__.py
+touch src/tools/__init__.py
+touch src/tools/job_connect.py
+
+
+echo "Directory and file created successfully"
